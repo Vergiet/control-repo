ca1bf5cd22b2fc2e666c72d872c590029a06ccff
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-05 16:33:41 +0200

    push

--- site-modules/role/manifests/ad.pp
+++
@@ -1,9 +0,0 @@
-class role::ad {
-  class { 'active_directory::domain_controller':
-    domain_name               => 'contoso.local',
-    domain_credential_user    => 'Administrator',
-    domain_credential_passwd  => 'Beheer123',
-    safe_mode_passwd          => 'Beheer123',
-  }
-}
-
