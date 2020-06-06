5cd2b3f3400e4da21c0803b26faa989c957a37ec
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-06 16:50:18 +0200

    asd

--- manifests/vmm.pp
+++ manifests/vmm.pp
@@ -8,9 +8,15 @@ class vmm::master (
     }
 
     file { 'vmminstaller':
+    subscribe => file['tempdir'],
       ensure => present,
       path => 'c:\\temp\\SCVMM_2019.exe',
       source => 'http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCVMM_2019.exe',
+    }
+
+    exec { 'resource title':
+      subscribe   => file['vminstaller'],
+      command     => 'c:\\temp\\SCVMM_2019.exe /SP- /silent /suppressmsgboxes',
     }
 
 /* 
