49a7b0e064fd4ab5003b9fcc5450fd3592748614
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-06 17:59:59 +0200

    asd

--- manifests/base_server.pp
+++ manifests/base_server.pp
@@ -6,7 +6,7 @@ class base::server {
     dsc_address => '192.168.1.131',
     dsc_validate => $true,
   }
-
+/* 
   class { domain_membership:
     domain => 'ad.contoso.com',
     username => 'Administrator@ad.contoso.com',
@@ -14,6 +14,17 @@ class base::server {
     machine_ou => 'OU=SERVERS,OU=ORG,DC=ad,DC=contoso,DC=com',
     reboot => true,
     join_options => '3',
+  }
+ */
+
+
+  dsc_computer { 'joindomain':
+    dsc_name => $facts['networking']['hostname'],
+    dsc_domainname => 'ad.contoso.com',
+    dsc_credential => {
+        'user'     => 'Administrator@ad.contoso.com',
+        'password' => Sensitive('Beheer123')
+      },
   }
 
 }
