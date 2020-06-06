144e4c31dc23940985257d96d39cdf6874aa40ca
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-06 10:36:06 +0200

    asd

---
+++ manifests/pdc.pp
@@ -0,0 +1,47 @@
+class ad::pdc (
+  String $ntds_dir = 'c:\NTDS',
+){
+  if $osfamily == 'windows' {
+
+    file {[$ntds_dir,]:
+      ensure   => directory,
+    }
+
+  /* 
+    dsc_file  {'adfile':
+      dsc_destinationpath => $ntds_dir
+      dsc_type => 'Directory',
+      dsc_ensure => 'Present',
+    } ->
+  */
+    dsc_windowsfeature  { 'addsinstall':
+      dsc_ensure => 'Present',
+      dsc_name   => 'AD-Domain-Services',
+    }
+
+    dsc_windowsfeature  {'addstools':
+      dsc_ensure => 'Present',
+      dsc_name   => 'RSAT-ADDS',
+    }
+
+    dsc_xaddomain   { 'firstdc':
+      subscribe                         => Dsc_windowsfeature['addsinstall'],
+      dsc_domainname                    => 'ad.contoso.com',
+      dsc_domainadministratorcredential => {
+        'user'     => 'pagent',
+        'password' => Sensitive('Test12341234')
+      },
+      dsc_safemodeadministratorpassword => {
+        'user'     => 'pagent',
+        'password' => Sensitive('Test12341234')
+      },
+      dsc_databasepath                  => $ntds_dir,
+      dsc_logpath                       => $ntds_dir,
+    }
+
+    reboot {'dsc_reboot':
+      message => 'DSC has requested a reboot',
+      when    => pending,
+    }
+  }
+}
