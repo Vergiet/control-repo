cdcbe19ed74a823f7e39ef94d70810c18073e28e
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-06 21:51:24 +0200

    asd

--- manifests/vmm.pp
+++ manifests/vmm.pp
@@ -39,58 +39,53 @@ class vmm::master (
       drive_letter => 'S',
     }
 
-/*
-    ACTION Download
-    LANGUAGE en-US
-    MEDIAPATH Microsoft SQL Server Install Media
-    QUIET
-    MEDIATYPE
+ 
+    dsc_sqlsetup {'dsc_sqlsetup':
+      dsc_InstanceName         => 'MSSQLSERVER',
+      dsc_Features             => 'SQLENGINE,AS',
+      dsc_SQLCollation         => 'SQL_Latin1_General_CP1_CI_AS',
+      dsc_SQLSvcAccount        => {
+        'user'     => 'Administrator@ad.contoso.com',
+        'password' => Sensitive('Beheer123'),
+      },
+      dsc_AgtSvcAccount        => {
+        'user'     => 'Administrator@ad.contoso.com',
+        'password' => Sensitive('Beheer123'),
+      },
+      dsc_ASSvcAccount         => {
+        'user'     => 'Administrator@ad.contoso.com',
+        'password' => Sensitive('Beheer123'),
+      },
+      dsc_SQLSysAdminAccounts  => 'AD\Domain Admins',
+      dsc_ASSysAdminAccounts   => 'AD\Domain Admins',
+      dsc_InstallSharedDir     => 'C:\\Program Files\\Microsoft SQL Server',
+      dsc_InstallSharedWOWDir  => 'C:\\Program Files (x86)\\Microsoft SQL Server',
+      dsc_InstanceDir          => 'C:\\Program Files\\Microsoft SQL Server',
+      dsc_InstallSQLDataDir    => 'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\Data',
+      dsc_SQLUserDBDir         => 'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\Data',
+      dsc_SQLUserDBLogDir      => 'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\Data',
+      dsc_SQLTempDBDir         => 'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\Data',
+      dsc_SQLTempDBLogDir      => 'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\Data',
+      dsc_SQLBackupDir         => 'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\Backup',
+      dsc_ASServerMode         => 'TABULAR',
+      dsc_ASConfigDir          => 'C:\\MSOLAP\\Config',
+      dsc_ASDataDir            => 'C:\\MSOLAP\\Data',
+      dsc_ASLogDir             => 'C:\\MSOLAP\\Log',
+      dsc_ASBackupDir          => 'C:\\MSOLAP\\Backup',
+      dsc_ASTempDir            => 'C:\\MSOLAP\\Temp',
+      dsc_SourcePath           => 'S:\\',
+      dsc_NpEnabled            => 'true',
+      dsc_TcpEnabled           => 'true',
+      dsc_UpdateEnabled        => 'False',
+      dsc_UseEnglish           => 'true',
+      dsc_ForceReboot          => 'False',
 
-
-
-
-    ACTION Download
-
-
-*/
-/* 
-                    dsc_sqlsetup {'dsc_sqlsetup':
-            InstanceName         = 'MSSQLSERVER'
-            Features             = 'SQLENGINE,AS'
-            SQLCollation         = 'SQL_Latin1_General_CP1_CI_AS'
-            SQLSvcAccount        = $SqlServiceCredential
-            AgtSvcAccount        = $SqlAgentServiceCredential
-            ASSvcAccount         = $SqlServiceCredential
-            SQLSysAdminAccounts  = 'COMPANY\SQL Administrators', $SqlAdministratorCredential.UserName
-            ASSysAdminAccounts   = 'COMPANY\SQL Administrators', $SqlAdministratorCredential.UserName
-            InstallSharedDir     = 'C:\Program Files\Microsoft SQL Server'
-            InstallSharedWOWDir  = 'C:\Program Files (x86)\Microsoft SQL Server'
-            InstanceDir          = 'C:\Program Files\Microsoft SQL Server'
-            InstallSQLDataDir    = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data'
-            SQLUserDBDir         = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data'
-            SQLUserDBLogDir      = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data'
-            SQLTempDBDir         = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data'
-            SQLTempDBLogDir      = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data'
-            SQLBackupDir         = 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup'
-            ASServerMode         = 'TABULAR'
-            ASConfigDir          = 'C:\MSOLAP\Config'
-            ASDataDir            = 'C:\MSOLAP\Data'
-            ASLogDir             = 'C:\MSOLAP\Log'
-            ASBackupDir          = 'C:\MSOLAP\Backup'
-            ASTempDir            = 'C:\MSOLAP\Temp'
-            SourcePath           = 'C:\InstallMedia\SQL2016RTM'
-            NpEnabled            = $true
-            TcpEnabled           = $true
-            UpdateEnabled        = 'False'
-            UseEnglish           = $true
-            ForceReboot          = $false
-
-            PsDscRunAsCredential = $SqlInstallCredential
-            subscribe => dsc_windowsfeature['NET-Framework-Core']
-                    }
-
-
- */
+      dsc_PsDscRunAsCredential => {
+        'user'     => 'Administrator@ad.contoso.com',
+        'password' => Sensitive('Beheer123'),
+      },
+      subscribe                => dsc_windowsfeature['NET-Framework-Core'],
+    }
 # 
 /* 
   dsc_xscvmmmanagementserversetup { 'vmminstall':
