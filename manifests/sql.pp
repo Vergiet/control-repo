1ddbef7456f925c4c0f573bd707d02c796a95946
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-07 12:41:33 +0200

    asd

--- manifests/sql.pp
+++ manifests/sql.pp
@@ -1,4 +1,4 @@
-class vmm::master (
+class sql::standalone (
   
 ) {
 
@@ -7,24 +7,10 @@ class vmm::master (
       path => 'c:\\temp\\',
     }
 
-    file { 'vmminstaller':
-      ensure => present,
-      subscribe => File['tempdir'],
-      path => 'c:\\temp\\SCVMM_2019.exe',
-      source => 'http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCVMM_2019.exe',
-    }
-
-    exec { 'extractvmm':
-      command     => 'c:\\temp\\SCVMM_2019.exe /SP- /silent /suppressmsgboxes',
-      subscribe   => File['vmminstaller'],
-      unless => 'C:\\Windows\\System32\\cmd.exe -c if exist "C:\\System Center Virtual Machine Manager\\setup.exe" (exit) else (exit 1) ',
-    }
-
     dsc_windowsfeature { "NET-Framework-45-Core":
         dsc_ensure => "Present",
         dsc_name => "NET-Framework-45-Core",
     }
-
 
     file { 'downloadsqlinstalleriso':
       ensure => present,
@@ -32,7 +18,6 @@ class vmm::master (
       path => 'c:\\temp\\SQLServer2019-x64-ENU.iso',
       source => 'https://dh2euwstodevinfinf01.blob.core.windows.net/temp/iso/SQLServer2019-x64-ENU.iso',
     }
-
 
     file { 'downloadsqlssmsinstaller':
       ensure => present,
@@ -73,42 +58,4 @@ class vmm::master (
       when    => pending,
     }
 
-# 
-/* 
-  dsc_xscvmmmanagementserversetup { 'vmminstall':
-    dsc_ensure => Present,
-    dsc_sourcepath => 'C:\\System Center Virtual Machine Manager\\',
-    dsc_setupcredential => {
-      'user' => 'Administrator',
-      'password' => Sensitive('Beheer123')
-    },
-    dsc_CreateNewSqlDatabase => 1,
-
-
-
-  }
-
- */
-/* 
-https://download.microsoft.com/download/4/8/6/486005eb-7aa8-4128-aac0-6569782b37b0/SQL2019-SSEI-Eval.exe
-http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCO_2019.exe
-http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCSM_2019.exe
-http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCSM_Auth_2019.exe
-http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCDPM_2019.exe
-http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCOM_2019.exe
-http://download.microsoft.com/download/C/4/E/C4E93EE0-F2AB-43B9-BF93-32E872E0D9F0/SCVMM_2019.exe
-https://download.microsoft.com/download/f/e/b/feb0e6be-21ce-4f98-abee-d74065e32d0a/SSMS-Setup-ENU.exe
-
- */
-
-
-
-/* 
-  dsc_dnsserveraddress { 'configurednsaddress':
-    dsc_interfacealias => $facts['networking']['primary'],
-    dsc_addressfamily => 'IPv4',
-    dsc_address => '192.168.1.131',
-    dsc_validate => $true,
-  } 
-*/
 }
