44e6fe787159049b726659aea48ce6519a152e99
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-07 12:55:13 +0200

    asd

--- manifests/vmm.pp
+++ manifests/vmm.pp
@@ -2,6 +2,8 @@ class vmm::master (
   
 ) {
 
+  require sql::standalone
+
     file { 'vmminstaller':
       ensure => present,
       #subscribe => File['vmm_tempdir'],
@@ -11,60 +13,9 @@ class vmm::master (
 
     exec { 'extractvmm':
       command     => 'c:\\temp\\SCVMM_2019.exe /SP- /silent /suppressmsgboxes',
-      #subscribe   => File['vmminstaller'],
+      subscribe   => File['vmminstaller'],
       unless => 'C:\\Windows\\System32\\cmd.exe -c if exist "C:\\System Center Virtual Machine Manager\\setup.exe" (exit) else (exit 1) ',
     }
-
-
-/* 
-    dsc_windowsfeature { "NET-Framework-45-Core":
-        dsc_ensure => "Present",
-        dsc_name => "NET-Framework-45-Core",
-    }
-
-
-    file { 'downloadsqlinstalleriso':
-      ensure => present,
-      subscribe => File['tempdir'],
-      path => 'c:\\temp\\SQLServer2019-x64-ENU.iso',
-      source => 'https://dh2euwstodevinfinf01.blob.core.windows.net/temp/iso/SQLServer2019-x64-ENU.iso',
-    }
-
-
-    file { 'downloadsqlssmsinstaller':
-      ensure => present,
-      subscribe => File['tempdir'],
-      path => 'c:\\temp\\SSMS-Setup-ENU.exe',
-      source => 'https://download.microsoft.com/download/f/e/b/feb0e6be-21ce-4f98-abee-d74065e32d0a/SSMS-Setup-ENU.exe',
-    }
-
-    mount_iso { 'C:\\temp\\SQLServer2019-x64-ENU.iso':
-      subscribe => File['downloadsqlinstalleriso'],
-      drive_letter => 'S',
-    }
-
-    dsc_sqlsetup { 'InstallDefaultInstance':
-      dsc_instancename        => 'MSSQLSERVER',
-      dsc_features            => 'SQLENGINE',
-      dsc_sourcepath          => 'S:\\',
-      dsc_sqlsysadminaccounts => 'Administrators',
-      subscribe               => [Dsc_windowsfeature['NET-Framework-45-Core'], Mount_iso['C:\\temp\\SQLServer2019-x64-ENU.iso']],
-    }
-
-    dsc_sqlwindowsfirewall { 'InstallDefaultInstancefw':
-        subscribe => Dsc_sqlsetup['InstallDefaultInstance'],
-        dsc_sourcepath => 'S:\\',
-        dsc_instancename => 'MSSQLSERVER',
-        dsc_features => 'SQLENGINE',
-    }
-
-    exec { 'installssms':
-      command     => 'start-process "c:\temp\SSMS-Setup-ENU.exe" -ArgumentList "/install", "/quiet", "/norestart" -NoNewWindow -Wait',
-      subscribe   => File['downloadsqlssmsinstaller'],
-      provider => 'powershell',
-      unless => 'if (Test-Path -Path "C:\\Program Files (x86)\\Microsoft SQL Server Management Studio 18" -PathType Container){exit} else {exit 1}',
-    }
-     */
 
     reboot {'vmm_dsc_reboot':
       message => 'DSC has requested a reboot',
