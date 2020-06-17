c6a95da877a905e2e18d6cb633f00cab5a2e3603
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-06-17 17:19:30 +0200

    update files

--- site-modules/mysqlconfig/manifests/database.pp
+++ site-modules/mysqlconfig/manifests/database.pp
@@ -2,5 +2,11 @@ class mysqlconfig::database {
 
   include mysql::server
 
+  service { 'mysql-service':
+    name => 'mariadb',
+    ensure     => running,
+    enable     => true,
+  }
+
   create_resources('mysql::db', hiera_hash('databases'))
 }
