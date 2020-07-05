2c4e407e99e28d767bd0bf44b3c2ce417fb886d0
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-07-05 14:01:56 +0200

    Update Files

--- manifests/nagiosxi.pp
+++ manifests/nagiosxi.pp
@@ -3,6 +3,7 @@ class nagiosxi::server::standalone {
   exec { 'curl https://assets.nagios.com/downloads/nagiosxi/install.sh | sh':
     timeout => 1800,
     path => ['/usr/bin', '/usr/sbin',],
+    unless => 'test -f /tmp/nagiosxi/install.log',
   }
 
 }
