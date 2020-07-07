5561a90c5b2d92df23d09b3aef3ac71d9038add6
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-07-07 17:12:00 +0200

    Update Files

--- modules/nagios/manifests/export.pp
+++ modules/nagios/manifests/export.pp
@@ -3,10 +3,10 @@ class nagios::export {
 
   nagios::resource { $::hostname:
     type => 'host',
-    address => inline_template("<%= has_variable?('my_nagios_interface') ?
-↪eval('ipaddress_' + my_nagios_interface) : ipaddress %>"),
-    hostgroups => inline_template("<%= has_variable?('my_nagios_hostgroups') ?
-↪$my_nagios_hostgroups : 'Other' %>"),
+    #address => inline_template("<%= has_variable?('my_nagios_interface') ? eval('ipaddress_' + my_nagios_interface) : ipaddress %>"),
+    address => $::ipaddress,
+    #hostgroups => inline_template("<%= has_variable?('my_nagios_hostgroups') ? $my_nagios_hostgroups : 'Other' %>"),
+    hostgroups => 'all-servers',
     check_command => 'check_host_alive!3000.0,80%!5000.0,100%!10',
     bexport => true,
   }
