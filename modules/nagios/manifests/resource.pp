298dc377e328ce3b71444e459875e89b8f5f7d34
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-08-10 18:56:28 +0200

    Update Files

--- modules/nagios/manifests/resource.pp
+++ modules/nagios/manifests/resource.pp
@@ -23,7 +23,7 @@ define nagios::resource(
   $event_handler = '',
   $flap_detection_enabled = '1',
   $flap_detection_options = 'o,d,u',
-  $low_flap_threshold = '0'
+  $low_flap_threshold = '0',
   $high_flap_threshold = '0',
   $process_perf_data = '1',
   $retain_status_information = '1',
