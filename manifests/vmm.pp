cf8317e617fd7820445ba25ecd2161d9fca6ac44
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-08-12 18:23:03 +0200

    Update Files

--- manifests/vmm.pp
+++ manifests/vmm.pp
@@ -38,7 +38,7 @@ class vmm::master (
 
 
     exec { 'installvmm':
-      command     => 'start-process "C:\\System Center Virtual Machine Manager\\setup.exe" -ArgumentList "/server", "/i", "/vmmservicedomain=\'contoso.com\'", "/vmmserviceUserName=\'administrator\'", "/vmmserviceuserpassword=\'Beheer123\'", "/SqlDBAdminDomain=\'contoso.com\'", "/SqlDBAdminName=\'administrator\'", "/SqlDBAdminpassword=\'Beheer123\'", "/IACCEPTSCEULA" -NoNewWindow -Wait',
+      command     => 'start-process "C:\\System Center Virtual Machine Manager\\setup.exe" -ArgumentList "/server", "/i", "/vmmservicedomain=\'mshome.net\'", "/vmmserviceUserName=\'administrator\'", "/vmmserviceuserpassword=\'Beheer123\'", "/SqlDBAdminDomain=\'mshome.net\'", "/SqlDBAdminName=\'administrator\'", "/SqlDBAdminpassword=\'Beheer123\'", "/IACCEPTSCEULA" -NoNewWindow -Wait',
       subscribe   => File['vmminstaller'],
       provider => 'powershell',
       unless => 'if (Test-Path -Path "C:\\Program Files\\Microsoft System Center\\Virtual Machine Manager" -PathType Container){exit} else {exit 1}',
