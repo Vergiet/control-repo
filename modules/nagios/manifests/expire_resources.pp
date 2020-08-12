f7ff2722f1e7c2393729b664bc5dba5ac356f450
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-08-12 07:48:46 +0200

    Update Files

--- modules/nagios/manifests/expire_resources.pp
+++ modules/nagios/manifests/expire_resources.pp
@@ -1,7 +1,7 @@
 class nagios::expire_resources {
 
   if $my_nagios_purge_hosts {
-    call_function('expire_exported', $my_nagios_purge_hosts),
+    call_function('expire_exported', $my_nagios_purge_hosts)
   }
 }
 
