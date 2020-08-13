0635353e8fa519b27f7fab8fcbf9bb7b7e7ee6cc
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-08-13 17:39:54 +0200

    Update Files

--- manifests/asd.pp
+++
@@ -1,4 +0,0 @@
-class profile::network::windows_install_dsc_modules {
-
-
-}
--- manifests/hv.pp
+++ manifests/hv.pp
@@ -1,6 +1,13 @@
 class hv::baseline (
 
 ) {
+ windowsfeature { 'Hyper-V':
+   ensure => present,
+ }
 
+ reboot {'after_Hyper_V':
+   when      => pending,
+   subscribe => Windowsfeature['Hyper-V'],
+ }
 
 }
