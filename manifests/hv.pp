971cc67fea0c1a475a6c5990578fd5225c93e998
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-08-13 20:19:39 +0200

    Update Files

--- manifests/hv.pp
+++ manifests/hv.pp
@@ -1,8 +1,17 @@
 class hv::baseline (
 
 ) {
+
+
+  reboot {'before_Hyper_V':
+   when      => pending,
+ }
+
+
+
  windowsfeature { 'Hyper-V':
    ensure => present,
+   require => Reboot['before_Hyper_V'],
  }
 
  reboot {'after_Hyper_V':
