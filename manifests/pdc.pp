6fb48eb426a75414d074602691b0d01137ac0e2f
daniel.hoornweg@ogd.nl <daniel.hoornweg@ogd.nl>
2020-09-10 15:13:23 +0200

    Update Files

--- manifests/pdc.pp
+++ manifests/pdc.pp
@@ -95,26 +95,6 @@ if ($IPv4DefaultGateway -ne (get-DnsServerForwarder).IPAddress.IPAddressToString
 
 '
 
-$configtasks = '
-$Scripts = @(
-    "C:\\scripts\\01_ensurednsforwarder.ps1"
-    "C:\\scripts\\02_ensurenagios.ps1"
-    "C:\\scripts\\03_ensurepmom01.ps1"
-)
-
-Foreach ($Script in $Scripts){
-
-    $Trigger= New-ScheduledTaskTrigger -AtStartup  # Specify the trigger settings
-    $User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
-    $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Script # Specify what program to run and with its parameters
-    Register-ScheduledTask -TaskName (split-path -Leaf $Script) -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force # Specify the name of the task
-
-
-}
-
-'
-
-
 $configureScavaging = '
 $GetDnsServerScavenging = Get-DnsServerScavenging 
 
@@ -153,53 +133,136 @@ if ($setDnsServerScavenging.Keys.Count -gt 0){
 }
 '
 
+$task = '
+$NetIPInterface = Get-NetIPInterface -InterfaceAlias "Default Switch" -AddressFamily IPv4
+$IPv4DefaultGateway = (Get-NetIPConfiguration -InterfaceIndex $NetIPInterface.InterfaceIndex).IPv4DefaultGateway.NextHop
+
+if ($IPv4DefaultGateway -ne (get-DnsServerForwarder).IPAddress.IPAddressToString){
+    
+    Set-DnsServerForwarder -IPAddress $IPv4DefaultGateway
+}
+
+$GetDnsServerScavenging = Get-DnsServerScavenging 
+
+$scavagingtime = (New-TimeSpan -Start (get-date).AddHours(-1) -End (get-date))
+
+$setDnsServerScavenging = @{}
+
+if ($GetDnsServerScavenging.ScavengingState -eq $False){
+
+    $setDnsServerScavenging.ScavengingState = $True
+}
+
+if ($GetDnsServerScavenging.RefreshInterval -ne $scavagingtime){
+
+    $setDnsServerScavenging.RefreshInterval = $scavagingtime
+}
 
 
-  file { "c:\\scripts\\ensuredns.ps1" :
+if ($GetDnsServerScavenging.RefreshInterval -ne $scavagingtime){
+
+    $setDnsServerScavenging.RefreshInterval = $scavagingtime
+}
+
+if ($GetDnsServerScavenging.ScavengingInterval -ne $scavagingtime){
+
+    $setDnsServerScavenging.ScavengingInterval = $scavagingtime
+}
+
+if ($GetDnsServerScavenging.NoRefreshInterval -ne $scavagingtime){
+
+    $setDnsServerScavenging.NoRefreshInterval = $scavagingtime
+}
+
+if ($setDnsServerScavenging.Keys.Count -gt 0){
+    Set-DnsServerScavenging -ApplyOnAllZones @setDnsServerScavenging -Verbose
+}
+
+Start-DnsServerScavenging -Force
+
+
+[array] $Names += [pscustomobject]@{name = "pmom01"; ip = ""}
+[array] $Names += [pscustomobject]@{name = "nagios"; ip = ""}
+[array] $Names += [pscustomobject]@{name = "Cluster01"; ip = "192.168.1.13"}
+
+$NetIPInterface = Get-NetIPInterface -InterfaceAlias "Default Switch" -AddressFamily IPv4
+$IPv4DefaultGateway = (Get-NetIPConfiguration -InterfaceIndex $NetIPInterface.InterfaceIndex).IPv4DefaultGateway.NextHop
+
+$Name = $Names[2]
+
+foreach ($Name in $Names){
+
+    $DnsName = ("{0}.mshome.net" -f $Name.name)
+  if ([string]::isnullorempty($Name.ip)){
+  
+  $IPAddress = (Resolve-DnsName -name $DnsName -Server $IPv4DefaultGateway).IPAddress
+  } else {
+    $IPAddress = $Name.ip
+  }
+
+
+
+  $DnsServerResourceRecord = Get-DnsServerResourceRecord -Name $Name.name -ZoneName mshome.net -ErrorAction SilentlyContinue
+
+  if ($null -eq $DnsServerResourceRecord -or $DnsServerResourceRecord.RecordData.IPv4Address.IPAddressToString -ne $IPAddress){
+
+      if ($null -ne $DnsServerResourceRecord){
+          Remove-DnsServerResourceRecord -ZoneName mshome.net -Name $Name.name -RRType "A" -Confirm:$False -force
+
+          #restart-computer
+      }
+
+      Add-DnsServerResourceRecordA -Name $Name.name -ZoneName mshome.net -IPv4Address $IPAddress
+
+  }
+
+}
+
+'
+
+$configtask = '
+
+$Script = "C:\scripts\Task.ps1"
+
+if (!(Get-ScheduledTask | ?{$_.TaskName -eq (split-path -Leaf $Script)})){
+
+    
+    $Trigger= New-ScheduledTaskTrigger -AtStartup  # Specify the trigger settings
+    $User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
+    $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Script # Specify what program to run and with its parameters
+    Register-ScheduledTask -TaskName (split-path -Leaf $Script) -Trigger $Trigger -User $User -Action $Action -RunLevel Highest 
+
+}
+'
+
+
+  file { "c:\\scripts\\configtask.ps1" :
     ensure   => present,
-    content => $ensuredns,
+    content => $configtask,
     require => File[$scripts_dir],
   }
 
-  file { "c:\\scripts\\configureScavaging.ps1" :
+
+  file { "c:\\scripts\\task.ps1" :
     ensure   => present,
-    content => $configureScavaging,
+    content => $task,
     require => File[$scripts_dir],
   }
 
-  file { "c:\\scripts\\01_ensurednsforwarder.ps1" :
-    ensure   => present,
-    content => $ensurednsforwarder,
-    require => File[$scripts_dir],
-  }
 
-  file { "c:\\scripts\\configtasks.ps1" :
-    ensure   => present,
-    content => $configtasks,
-    require => File[$scripts_dir],
-  }
-
-  exec { 'configtasks':
-    command     => '& c:\\scripts\\configtasks.ps1',
-    subscribe   => File['c:\\scripts\\configtasks.ps1'],
+  exec { 'task' :
+    command     => '& c:\\scripts\\task.ps1',
+    subscribe   => File['c:\\scripts\\task.ps1'],
     provider => 'powershell',
-    require => Dsc_xaddomain['firstdc'],
+    require => [Dsc_xaddomain['firstdc'], File["c:\\scripts\\task.ps1"]],
   }
 
-  exec { 'ensuredns' :
-    command     => '& c:\\scripts\\ensuredns.ps1',
-    subscribe   => File['c:\\scripts\\ensuredns.ps1'],
+  exec { 'configtask' :
+    command     => '& c:\\scripts\\configtask.ps1',
+    subscribe   => File['c:\\scripts\\configtask.ps1'],
     provider => 'powershell',
-    require => [Dsc_xaddomain['firstdc'], File["c:\\scripts\\ensuredns.ps1"]],
+    require => [Dsc_xaddomain['firstdc'], File["c:\\scripts\\configtask.ps1"], File["c:\\scripts\\task.ps1"]],
   }
-
-  exec { 'configureScavaging' :
-    command     => '& c:\\scripts\\configureScavaging.ps1',
-    subscribe   => File['c:\\scripts\\configureScavaging.ps1'],
-    provider => 'powershell',
-    require => [Dsc_xaddomain['firstdc'], File["c:\\scripts\\configureScavaging.ps1"]],
-  }
-
 
   reboot {'dsc_reboot':
     message => 'DSC has requested a reboot',
