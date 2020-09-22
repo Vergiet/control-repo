f128fe4f8251eac884a2bd909302d30d63ffe590
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-09-22 10:06:59 +0200

    Update Files

--- manifests/hv.pp
+++ manifests/hv.pp
@@ -123,14 +123,14 @@ if ((Get-Cluster -Name cluster02).S2DEnabled -ne 0){
       Get-StoragePool | ? IsPrimordial -eq $false | Get-VirtualDisk | Remove-VirtualDisk -Confirm:$false #-ErrorAction SilentlyContinue
       Get-StoragePool | ? IsPrimordial -eq $false | Remove-StoragePool -Confirm:$false #-ErrorAction SilentlyContinue
       Get-PhysicalDisk | Reset-PhysicalDisk #-ErrorAction SilentlyContinue
-      Get-Disk | ? Number -ne $null | ? IsBoot -ne $true | ? IsSystem -ne $true | ? PartitionStyle -ne RAW | % {
+      Get-Disk | ? Number -ne $null | ? IsBoot -ne $true | ? IsSystem -ne $true | ? PartitionStyle -ne "RAW" | % {
           $_ | Set-Disk -isoffline:$false
           $_ | Set-Disk -isreadonly:$false
           $_ | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false
           $_ | Set-Disk -isreadonly:$true
           $_ | Set-Disk -isoffline:$true
       }
-      Get-Disk | Where Number -Ne $Null | Where IsBoot -Ne $True | Where IsSystem -Ne $True | Where PartitionStyle -Eq RAW | Group -NoElement -Property FriendlyName
+      Get-Disk | Where Number -Ne $Null | Where IsBoot -Ne $True | Where IsSystem -Ne $True | Where PartitionStyle -Eq "RAW" | Group -NoElement -Property FriendlyName
   } | Sort -Property PsComputerName, Count
 
 
@@ -138,7 +138,7 @@ if ((Get-Cluster -Name cluster02).S2DEnabled -ne 0){
 
   Enable-ClusterStorageSpacesDirect –CimSession Cluster02
 
-  New-Volume -FriendlyName "Volume1" -FileSystem CSVFS_ReFS -StoragePoolFriendlyName S2D* -Size 100GB
+  New-Volume -FriendlyName "Volume1" -FileSystem CSVFS_ReFS -StoragePoolFriendlyName "S2D*" -Size 100GB
 
   $ClusterName = "Cluster02"
   $CSVCacheSize = 2048 #Size in MB
