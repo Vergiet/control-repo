c1a0bcdc93aea493701c70678b37f347f8f865bf
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-09-24 08:44:49 +0200

    Update Files

--- manifests/ras.pp
+++ manifests/ras.pp
@@ -29,6 +29,26 @@ class ras::multitenant (
     require => [Windowsfeature['RSAT-RemoteAccess-PowerShell'], Reboot['after-reboot']],
   }
 
+$setipaddress = '
+$ip = (Get-NetAdapter -Name "Default Switch" | Get-NetIPAddress -AddressFamily IPv4).IPAddress.split(".")[3]
+
+if (!(Get-NetIPAddress -InterfaceIndex (Get-NetAdapter -Name "Provider").interfaceindex | ?{$_.ipAddress -eq "10.0.0.$ip"})){
+
+  New-NetIPAddress -IPAddress "10.0.0.$ip" -InterfaceIndex (Get-NetAdapter -Name "Provider").interfaceindex -PrefixLength 24 -verbose
+}
+'
+
+  file { "c:\\scripts\\setipaddress.ps1" :
+    ensure   => present,
+    content => $setipaddress,
+  }
+
+
+  exec { 'setipaddress':
+    command     => '& c:\\scripts\\setipaddress.ps1',
+    require   => File['c:\\scripts\\setipaddress.ps1'],
+    provider => 'powershell',
+  }
 
 
 }
