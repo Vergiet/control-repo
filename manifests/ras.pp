4ef28832e2c3a513d9fd9cb1e80cae639e12447c
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-09-24 08:26:36 +0200

    Update Files

--- manifests/ras.pp
+++ manifests/ras.pp
@@ -3,11 +3,25 @@ class ras::multitenant (
 ) {
 
 
+  reboot {'pre-reboot':
+    message => 'DSC has requested a reboot before installing roles',
+    when    => pending,
+  }
+
   $rasroles = ["RemoteAccess","DirectAccess-VPN","Routing"]
 
   windowsfeature { $rasroles:
     ensure => present,
     installsubfeatures => true,
+    require => Reboot['pre-reboot'],
+    notify => Reboot['after-reboot']
   }
+
+  reboot {'after-reboot':
+    message => 'DSC has requested a reboot after installing roles',
+    when    => pending,
+  }
+
+
 
 }
