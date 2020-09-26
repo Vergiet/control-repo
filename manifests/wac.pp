4e49f3af39da49a402a34f336d86ae31b395c939
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-09-26 12:20:33 +0200

    Update Files

--- manifests/wac.pp
+++ manifests/wac.pp
@@ -12,7 +12,7 @@ class wac (
       command     => 'msiexec /i c:\\temp\\WindowsAdminCenter2007.msi /qn /L*v log.txt SME_PORT=443 SSL_CERTIFICATE_OPTION=generate',
       subscribe   => File['c:\\temp\\WindowsAdminCenter2007.msi'],
       provider => 'powershell',
-      #unless => 'if (Test-Path -Path "C:\\Program Files\\Microsoft System Center\\Virtual Machine Manager" -PathType Container){exit} else {exit 1}',
+      unless => 'if (Test-Path -Path "c:\\Program Files\\Windows Admin Center" -PathType Container){exit} else {exit 1}',
       require => File['c:\\temp\\WindowsAdminCenter2007.msi'],
     }
 
