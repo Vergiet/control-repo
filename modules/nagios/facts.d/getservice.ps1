f81c05fb1ae56e6cff62ab3a0261d66e4b8821e4
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-10-10 11:47:40 +0200

    Update Files

--- modules/nagios/facts.d/getservice.ps1
+++ modules/nagios/facts.d/getservice.ps1
@@ -1,1 +1,7 @@
-write-output "services=$(get-service | select name,DisplayName,ServiceName,Status,StartType | ConvertTo-Json -Depth 4 | out-string)"
+#write-output "services=$(get-service | select name,DisplayName,ServiceName,Status,StartType -first 4 | ConvertTo-Json -Depth 4)"
+
+foreach ($Service in (get-service )){
+
+    write-output "{0}={1}" -f $Service.ServiceName,($Service | select name,DisplayName,ServiceName,Status,StartType | ConvertTo-Json -Depth 4)
+
+}
