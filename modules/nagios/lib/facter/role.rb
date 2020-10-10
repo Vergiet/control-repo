a800eccf619dcc8caf14e1933bc3b8b7eeafbcc0
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-10-10 11:01:56 +0200

    Update Files

--- modules/nagios/lib/facter/role.rb
+++ modules/nagios/lib/facter/role.rb
@@ -1,19 +1,10 @@
-Facter.add('windows_edition_custom') do
-    confine :osfamily => :windows
-    setcode do
-      value = nil
-      Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\Windows NT\CurrentVersion') do |regkey|
-        value = regkey['EditionID']
-      end
-      value
-    end
-  end
-
 Facter.add('services') do
     confine :osfamily => :windows
     setcode do
       value = nil
-        value = Win32::Service.services
+      value = Array.new 
+      Win32::Service.services do |service|
+        value << service
       end
       value
     end
