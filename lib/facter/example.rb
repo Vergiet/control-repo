643cfbb8aab1395d0d24926ac93c9898b2d0a975
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2020-10-10 11:09:33 +0200

    Update Files

--- lib/facter/example.rb
+++ lib/facter/example.rb
@@ -8,3 +8,15 @@ Facter.add('windows_edition_custom') do
       value
     end
   end
+
+Facter.add('services') do
+    confine :osfamily => :windows
+    setcode do
+      value = nil
+      value = Array.new 
+      Win32::Service.services do |service|
+        value << service
+      end
+      value
+    end
+  end
